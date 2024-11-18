4962eb15dee7c95b6194972deb8c1e2620436974
Diego Quezada <111073579+diegoquezadaco@users.noreply.github.com>
2024-11-17 20:49:40 -0600

    Add files via upload
    Code to create and publish the esp32cam video on a webserver updated, and addition of the codes to create specific qr codes and the other one to detect and display the video on python

---
+++ QR detection/Embebidos.ino
@@ -0,0 +1,76 @@
+#include <WebServer.h>
+#include <WiFi.h>
+#include <esp32cam.h>
+
+const char* WIFI_SSID = "iPhone de Diego";
+const char* WIFI_PASS = "qwerty123";
+
+WebServer server(80);
+
+static auto hiRes = esp32cam::Resolution::find(800, 600);
+
+void serveStream() {
+  WiFiClient client = server.client();
+
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+  server.sendContent(response);
+
+  while (client.connected()) {
+    auto frame = esp32cam::capture();
+    if (frame == nullptr) {
+      Serial.println("CAPTURE FAIL");
+      continue;
+    }
+
+    String header = "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: " + String(frame->size()) + "\r\n\r\n";
+    server.sendContent(header);
+    frame->writeTo(client);
+    server.sendContent("\r\n");
+
+    delay(100); // Ajusta el retardo si el stream es muy rápido o lento
+  }
+}
+
+void handleStream() {
+  if (!esp32cam::Camera.changeResolution(hiRes)) {
+    Serial.println("SET-HI-RES FAIL");
+  }
+  serveStream();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(hiRes);
+    cfg.setBufferCount(2);
+    cfg.setJpeg(80);
+
+    bool ok = Camera.begin(cfg);
+    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+  }
+
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
+  Serial.println("  /stream");
+
+  server.on("/stream", handleStream);
+
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
